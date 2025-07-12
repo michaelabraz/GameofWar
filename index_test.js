ff16aca12245b1a79d82289ba24d82cf58ae9975
Michaela Brazington <michaelabraz@gmail.com>
2025-07-12 00:35:07 -0500

    adding tests

---
+++ index_test.js
@@ -0,0 +1,13 @@
+import { FullCardDeck } from "./cardDeck";
+import { expect } from "chai";
+
+describe('gameOfWarFunctions', function() {
+    describe('#makeDeck', function() {
+        it('should have 52 cards', function() {
+            // var fullDeck = new FullCardDeck;
+            // console.log('test is trying to run now');
+            // var testDeck = fullDeck.makeDeck();
+            // expect(testDeck).length.to.equal(52);
+        })
+    })
+})
